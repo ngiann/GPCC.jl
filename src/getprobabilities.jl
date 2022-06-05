e80779e639a79324bed26e93e1a327540ee8bb6c
ngiann <ngiann@gmail.com>
2022-06-05 21:06:31 +0200

    added commented out code for alternative posterior prob calculations

--- src/getprobabilities.jl
+++ src/getprobabilities.jl
@@ -7,5 +7,15 @@ function getprobabilities(out)
     aux = vec(mean(reduce(hcat, [exp.(Q[:,i] .- logsumexp(Q[:,i])) for i in 1:nfolds]), dims=2))
 
     reshape(aux, size(out))
-    
+
 end
+
+# function getprobabilities(cvresults)
+#
+#     aux = mean.(cvresults)
+#
+#     pr = exp.(aux .- logsumexp(aux))
+#
+#     pr / sum(pr) # sometimes pr will not sum up to 1 exactly, this line helps
+#
+# end
